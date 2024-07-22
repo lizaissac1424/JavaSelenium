d870a006f8d88a5b8e42ce3fa78e6f8464c374fd
lizaissac1424 <lizaissac1424@gmail.com>
2024-07-22 15:31:06 -0400

    Add files via upload

---
+++ JavaSelenium/src/main/java/week1/day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion =100.2f;
+		String browserName ="firefox";
+		boolean isVisible =true;
+		int releaseYear =1998;
+		char browserLogo ='f';
+		System.out.println("The Browser Version is : "+browserVersion+"\n"+"The Browser Name is : "+browserName+"\n"+"Is it Visible : "+isVisible+"\n"+"The Release Year : "+releaseYear+"\n"+"The Browser Logo : "+browserLogo);
+			
+
+	}
+
+}
---
+++ JavaSelenium/src/main/java/week1/day2/FindOddNumbers.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		int count=10;
+		int i;
+		for(i=1;i<=count;i++)
+		if(i%2==1)
+		{
+		System.out.println("The odd number is : "+i);
+		
+		}
+	}
+	
+}
