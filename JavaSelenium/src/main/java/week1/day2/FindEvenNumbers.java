fa6005032efc249e1931de1c1ba89855b71e07ad
lizai <lizai@LAPTOP-ONC4FDFU>
2024-07-22 23:08:09 -0500

    Added even number and prime number code

---
+++ JavaSelenium/src/main/java/week1/day2/FindEvenNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		int count=10;
+		int i;
+		for(i=1;i<=count;i++)
+		if(i%2==0)
+		{
+		System.out.println("The even number is : "+i);
+		
+		}
+
+	}
+
+}
---
+++ JavaSelenium/src/main/java/week1/day2/IsPrimeNumber.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class IsPrimeNumber {
+
+	public static void main(String[] args) {
+		int count=10;
+		int i;
+		for(i=1;i<=count;i++)
+		if(i%2!=0)
+		{
+		System.out.println("The prime number is : "+i);
+		
+		}
+
+	}
+
+}
